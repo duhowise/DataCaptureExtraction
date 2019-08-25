c5bb1ad361040ff80ccb01905489237a0b8fcd4f
Duho Wise <Duhowise@gmail.com>
2019-08-25 18:08:21 +0000

    Initial Commit

---
+++ EmailProcessing/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Pop3;
+using MimeKit;
+
+namespace EmailProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                EmailExample.ShowPop3Subjects();
+            }
+            catch (Exception exception)
+            {
+
+                Console.WriteLine(exception.StackTrace);
+            }
+        }
+    }
+    public class EmailExample
+    {
+        private const string cPopUserName = "test@popserver.com";
+        private const string cPopPwd = "testPwd123";
+        private const string cPopMailServer = "mail.popserver.com";
+        private const int cPopPort = 110;
+        public static void ShowPop3Subjects()
+        {
+            using (EmailParser ep =new EmailParser(cPopUserName,cPopPwd, cPopMailServer, cPopPort))
+            {
+                ep.OpenPop3();
+                ep.DisplayPop3Subjects();
+                ep.ClosePop3();
+            }
+        }
+    }
+}
+
+public class EmailParser : IDisposable
+    {
+        protected string User { get; set; }
+        protected string Password { get; set; }
+        protected string MailServer { get; set; }
+        protected int Port { get; set; }
+        public Pop3Client Pop3 { get; set; }
+
+        public EmailParser(string user, string password, string mailServer, int port)
+        {
+            User = user;
+            Password = password;
+            MailServer = mailServer;
+            Port = port;
+            Pop3 = null;
+        }
+
+    
+
+    public void OpenPop3()
+        {
+            if (Pop3==null)
+            {
+                Pop3=new Pop3Client();
+                Pop3.Connect(this.MailServer,this.Port,false);
+                Pop3.AuthenticationMechanisms.Remove("XOAUTH2");
+                Pop3.Authenticate(this.User,this.Password);
+            }
+        }
+
+        public void ClosePop3()
+        {
+            if (Pop3!=null)
+            {
+                Pop3.Disconnect(true);
+                Pop3.Dispose();
+                Pop3 = null;
+            }
+        }
+
+        public void DisplayPop3Subjects()
+        {
+            for (int i = 0; i < Pop3?.Count; i++)
+            {
+                MimeMessage message = Pop3.GetMessage(i);
+                Console.WriteLine($"Subject: {message.Subject}");
+            }
+        }
+
+
+        public void Dispose()
+        {
+            
+        }
+    }
+
---
+++ WebProcessing/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace WebProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bs=WebExample.GetNytBestSellersListNames();
+            for (int i = 0; i < bs.num_results; i++)
+            {
+                Console.WriteLine(bs.results[i].display_name);
+            }
+            Console.ReadLine();
+        }
+    }
+    public class BestSellersListNames
+    {
+        public string status;
+        public string copyright;
+        public int num_results;
+        public BestSellerListNamesItem[] results;
+    }
+    public class BestSellerListNamesItem
+    {
+        public string list_name;
+        public string display_name;
+        public string list_name_encoded;
+        public DateTime oldest_published_date;
+        public DateTime newest_published_date;
+        public string updated;
+    }
+    public class WebExample
+    {
+        
+        public const string cStrNYTBooksBaseUrl ="http://api.nytimes.com/svc/books/";
+        public const string cStrNYTBooksResource = "v3/lists/names.json";
+        public const string cStrNYTApiKeyStr = "api-key";
+        public const string cStrNYTApiKeyVal = "9188ea453d814d979d832ef840b07037";
+        public static BestSellersListNames GetNytBestSellersListNames()
+        {
+            BestSellersListNames bs = null;
+            string res = String.Empty;
+            using (WebParser wp = new WebParser(cStrNYTBooksBaseUrl))
+            {
+                res = wp.Request(cStrNYTBooksResource, Method.GET,cStrNYTApiKeyStr, cStrNYTApiKeyVal);
+                bs = wp.DeserializeJson<BestSellersListNames>(res);
+               }
+            return bs;
+        }
+       
+       
+    }
+    public class WebParser:IDisposable
+    {
+
+        protected RestClient WebClient = null;
+        protected string response=String.Empty;
+        public string Response { get { return response; } }
+
+        public string Request(string resource,Method type,string param,string value)
+        {
+            string res = string.Empty;
+            if (WebClient!=null)
+            {var request=new RestRequest(resource,type);
+                request.AddParameter(param, value);
+                IRestResponse htmlRes = WebClient.Execute(request);
+                res = htmlRes.Content;
+                if (res != null)
+                    response = res;
+            }
+            return res;
+        }
+        public T DeserializeJson<T>(string res)
+        {
+            return JsonConvert.DeserializeObject<T>(res);
+        }
+        protected bool disposed;
+
+        public WebParser(string baseUrl)
+        {
+            WebClient=new RestClient(baseUrl);
+        }
+
+        ~WebParser() { this.Dispose(false);}
+        
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                    WebClient = null;
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
